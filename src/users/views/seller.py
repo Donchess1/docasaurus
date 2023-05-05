8e754c1360272d0c3a207144289c4ac62946b309
Tosin Ayodele <devtosxn@gmail.com>
2023-05-05 05:21:26 +0100

    Add error field for kyc meta description

--- src/users/views/seller.py
+++ src/users/views/seller.py
@@ -47,7 +47,7 @@ class RegisterSellerView(CreateAPIView):
             return Response(
                 success=False,
                 status_code=status.HTTP_400_BAD_REQUEST,
-                errors=kyc_meta_serializer.errors,
+                errors={"kyc_meta": kyc_meta_serializer.errors},
             )
 
         self.perform_create(serializer)
