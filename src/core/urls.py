f47e8a25b84abbf59edc8be7aa8a3ee0ee4f38b2
Tosin Ayodele <devtosxn@gmail.com>
2023-09-01 04:53:12 +0100

    Add docs

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(
-        "",
+        "docs",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="swagger-schema-ui",
     ),
