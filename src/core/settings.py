dce7d32392a8e40f0883d98d57803ab19a8396aa
Tosin Ayodele <devtosxn@gmail.com>
2023-09-12 13:31:30 +0100

    update allowed frontend hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -78,6 +78,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://mybalanceapp.com",
     "https://mybalanceapp.com",
     "https://dapper-eclair-4a6866.netlify.app",
+    "http://dapper-eclair-4a6866.netlify.app",
+    "https://main--fastidious-bombolone-2dabe2.netlify.app",
+    "http://main--fastidious-bombolone-2dabe2.netlify.app",
 ]
 
 CORS_ORIGIN_WHITELIST = [
