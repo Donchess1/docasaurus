ad52095dd38d1ccbbf074d96d1ad8f4f5f0ec2e6
Tosin Ayodele <devtosxn@gmail.com>
2024-02-16 11:48:37 +0100

    Fix ZeehAfrica Integration for API

--- src/core/resources/zeeh_africa/main.py
+++ src/core/resources/zeeh_africa/main.py
@@ -4,5 +4,8 @@ from .base import ZeehAfricaServiceBaseService
 class ZeehAfricaService(ZeehAfricaServiceBaseService):
     @classmethod
     def validate_nin(cls, number):
-        url = f"{cls.ZEEHAFRICA_BASE_URL}/nin/live/lookup/{cls.ZEEHAFRICA_PUBLIC_KEY}?nin={number}"
-        return cls.make_get_request(url, zha_headers=cls.ZEEHAFRICA_HEADERS)
+        url = f"{cls.ZEEHAFRICA_BASE_URL}/nigeria_kyc/lookup_nin"
+        data = {"nin": number}
+        return cls.make_post_request(
+            url, data, camelize=False, zha_headers=cls.ZEEHAFRICA_HEADERS
+        )
