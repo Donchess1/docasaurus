963c37ed94ac23daf4387794b7af01f517bff3f2
tosxnpy <oluwatosin@bepeerless.co>
2024-03-07 12:14:40 +0100

    Email validation & Delivery Date

--- src/utils/email.py
+++ src/utils/email.py
@@ -1,6 +1,11 @@
+import os
+
+ENVIRONMENT = os.environ.get("ENVIRONMENT", None)
+env = "live" if ENVIRONMENT == "production" else "test"
+
+
 def validate_email_body(value):
-    return (
-        [True, "Invalid email address. Cannot contain '+'"]
-        if "+" in value
-        else [False, None]
-    )
+    if env == "live" and "+" in value:
+        return [True, "Invalid email address. Cannot contain '+'"]
+    else:
+        return [False, None]
