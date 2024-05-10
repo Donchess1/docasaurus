b50f06983909db0491658b9d2845454ef8fe4b62
tosxnpy <oluwatosin@bepeerless.co>
2024-05-10 21:55:23 +0100

    Return email from token trim

--- src/common/views/merchant.py
+++ src/common/views/merchant.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, permissions, status
 
@@ -5,13 +6,17 @@ from common.serializers.merchant import (
     TrimMerchantTokenPayloadSerializer,
     TrimMerchantTokenSerializer,
 )
+from core.resources.jwt_client import JWTClient
 from utils.response import Response
 from utils.utils import deconstruct_merchant_widget_key
+
+User = get_user_model()
 
 
 class TrimMerchantTokenView(generics.GenericAPIView):
     serializer_class = TrimMerchantTokenSerializer
     permission_classes = (permissions.AllowAny,)
+    jwt_client = JWTClient
 
     @swagger_auto_schema(
         operation_description="Deconstruct Merchant Widget Token",
@@ -30,13 +35,24 @@ class TrimMerchantTokenView(generics.GenericAPIView):
         data = serializer.validated_data
         key = data.get("key", None)
         payload = deconstruct_merchant_widget_key(key)
+        token = payload.get("token", None)
         if not payload:
             return Response(
                 success=False,
                 message="Invalid key provided",
                 status_code=status.HTTP_400_BAD_REQUEST,
             )
-
+        user_id = self.jwt_client.authenticate_token(token)
+        user, customer_email = None, None
+        if not user_id:
+            return Response(
+                success=False,
+                message="Invalid token",
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+        user = User.objects.filter(id=user_id).first()
+        customer_email = user.email
+        payload["customer_email"] = customer_email
         return Response(
             success=True,
             message="Conversion complete.",
