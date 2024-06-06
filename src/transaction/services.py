dda46055afa3530543e43bb9a8ed9ca9ded4daf6
tosxnfmp <tosin@formpl.us>
2024-06-06 13:41:54 +0100

    format files

--- src/transaction/services.py
+++ src/transaction/services.py
@@ -1,23 +1,24 @@
-from utils.transaction import get_escrow_transaction_users
 from console.models.transaction import EscrowMeta, LockedAmount, Transaction
+from utils.transaction import get_escrow_transaction_users
+
 
 def get_escrow_transaction_parties_info(transaction: Transaction) -> dict:
-        users = get_escrow_transaction_users(transaction)
-        buyer = users.get("BUYER")
-        seller = users.get("SELLER")
-        merchant = users.get("MERCHANT")
-        parties = {
-            "buyer": {
-                "name": buyer["name"] if buyer else "",
-                "email": buyer["email"] if buyer else "",
-            },
-            "seller": {
-                "name": seller["name"] if seller else "",
-                "email": seller["email"] if seller else "",
-            },
-            "merchant": {
-                "name": merchant["name"] if merchant else "",
-                "email": merchant["email"] if merchant else "",
-            },
-        }
-        return parties
+    users = get_escrow_transaction_users(transaction)
+    buyer = users.get("BUYER")
+    seller = users.get("SELLER")
+    merchant = users.get("MERCHANT")
+    parties = {
+        "buyer": {
+            "name": buyer["name"] if buyer else "",
+            "email": buyer["email"] if buyer else "",
+        },
+        "seller": {
+            "name": seller["name"] if seller else "",
+            "email": seller["email"] if seller else "",
+        },
+        "merchant": {
+            "name": merchant["name"] if merchant else "",
+            "email": merchant["email"] if merchant else "",
+        },
+    }
+    return parties
