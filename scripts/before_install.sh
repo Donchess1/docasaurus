7dc8840d3c2ce0ecb3c05a4a3aab455f5e9d3cb7
tosxnpy <oluwatosin@bepeerless.co>
2024-09-17 21:04:36 +0100

    update code deploy scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,10 +7,9 @@ echo "Starting before_install.sh script"
 # Fetch ECR_REGISTRY from Parameter Store
 ECR_REGISTRY=$(aws ssm get-parameter --name "/mybalance/global/ECR_REGISTRY" --with-decryption --query Parameter.Value --output text)
 echo "ECR_REGISTRY fetched: $ECR_REGISTRY"
-
 # Login to ECR
 echo "Attempting to log in to ECR"
-if aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $ECR_REGISTRY; then
+if aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY; then
     echo "Successfully logged in to ECR"
 else
     echo "Failed to log in to ECR. Error code: $?"
