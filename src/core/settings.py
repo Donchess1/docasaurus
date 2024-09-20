db21ec32a729c708881bd2cf60a5c2cc997d11ce
tosxnpy <oluwatosin@bepeerless.co>
2024-09-20 22:32:38 +0100

    update redis config v2

--- src/core/settings.py
+++ src/core/settings.py
@@ -222,7 +222,7 @@ LOGGING = {
 
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "PASSWORD")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
 
 CELERY_BROKER_URL = os.environ.get(
     "CELERY_BROKER", f"redis://{REDIS_HOST}:{REDIS_PORT}"
