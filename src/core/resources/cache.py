f52ea3e0f0f1bd671fd156c2d428b30f8e415a42
tosxnpy <oluwatosin@bepeerless.co>
2024-09-23 06:35:11 +0100

    Refactor Redis initialization to Singleton pattern

--- src/core/resources/cache.py
+++ src/core/resources/cache.py
@@ -1,29 +1,107 @@
 import json
+import os
 
 import redis
 from django.conf import settings
 
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
+
 
 class Cache:
-    def __init__(self):
-        # self._redis = redis.StrictRedis(
-        #     host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0
-        # )
-        # self._redis = redis.StrictRedis(
-        #     host=settings.REDIS_HOST,
-        #     port=settings.REDIS_PORT,
-        #     password=settings.REDIS_PASSWORD,
-        #     db=0,
-        #     ssl=True,
-        #     ssl_cert_reqs=None,
-        #     decode_responses=True
-        # )
-        self._redis = redis.Redis(
-            host=settings.REDIS_HOST,
-            port=settings.REDIS_PORT,
-            password=settings.REDIS_PASSWORD,
-            db=0,
-        )
+    """
+    A singleton class that provides an interface to interact with a Redis cache.
+
+    This class manages Redis connections based on the environment (development or production)
+    and provides common cache operations like setting, getting, deleting, and managing expiry.
+
+    The class implements a singleton pattern, ensuring that only one Redis connection is maintained
+    throughout the lifetime of the application, reducing unnecessary overhead from multiple connections.
+
+    Attributes:
+    -----------
+    _instance : Cache
+        Singleton instance of the Cache class, used to manage a single Redis connection.
+    _redis : redis.Redis
+        Redis client instance for interacting with the Redis server.
+
+    Methods:
+    --------
+    set(key, value, ttl=3600):
+        Set a value in the Redis cache for a given key with an optional time-to-live (TTL).
+
+    get(key):
+        Retrieve the value associated with a given key from the Redis cache.
+
+    delete(key):
+        Delete a key-value pair from the Redis cache.
+
+    flush():
+        Flush (clear) the entire Redis database.
+
+    expiry_time(key):
+        Get the remaining time-to-live (TTL) for a given key in seconds.
+
+    set_expiry(key, ttl):
+        Set or update the expiration time for a given key in the cache.
+
+    Usage:
+    ------
+    # Creating or retrieving the singleton Cache instance
+    cache = Cache()
+
+    # Storing a value in the cache
+    cache.set("banks", ["Bank A", "Bank B"])
+
+    # Retrieving a value from the cache
+    banks = cache.get("banks")
+
+    # Deleting a value from the cache
+    cache.delete("banks")
+
+    # Flushing the entire cache
+    cache.flush()
+
+    Notes:
+    ------
+    - The class automatically selects the appropriate Redis connection configuration based on the
+      environment (`ENVIRONMENT` variable).
+    - In development, it connects to a local Redis instance, while in staging/production, it connects using
+      credentials from the application settings.
+    - This class should be used with context management (`with` statement) to ensure connections are
+      closed properly.
+    """
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Cache, cls).__new__(cls)
+            if ENVIRONMENT == "development":
+                print("DEVELOPMENT")
+                cls._instance._redis = redis.Redis(
+                    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0
+                )
+            else:
+                print("STAGING/PRODUCTION")
+                cls._instance._redis = redis.Redis(
+                    host=settings.REDIS_HOST,
+                    port=settings.REDIS_PORT,
+                    password=settings.REDIS_PASSWORD,
+                    db=0,
+                )
+                # self._redis = redis.StrictRedis(
+                #     host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0
+                # )
+                # self._redis = redis.StrictRedis(
+                #     host=settings.REDIS_HOST,
+                #     port=settings.REDIS_PORT,
+                #     password=settings.REDIS_PASSWORD,
+                #     db=0,
+                #     ssl=True,
+                #     ssl_cert_reqs=None,
+                #     decode_responses=True
+                # )
+        return cls._instance
 
     def set(self, key, value, ttl=3600):
         value = json.dumps(value)
