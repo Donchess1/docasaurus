e038e246b0467b7ad46d57d38fabb9294ad525c6
Donchess1 <stepabod@yahoo.com>
2024-11-06 21:36:11 +0100

    reviewing serializer

--- src/blog/serializers.py
+++ src/blog/serializers.py
@@ -7,11 +7,12 @@ class BlogPostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source="author.name")
     reading_time = serializers.ReadOnlyField()
     markdown_file = serializers.FileField(write_only=True, required=True)
+    cover_image = serializers.ImageField()
 
     class Meta:
         model = BlogPost
         fields = ["tags","id","title","content","reading_time", "cover_image_url", "author", "markdown_file",
-                  "created_at", "updated_at", "deleted_at", "is_archived","is_draft"]
+                  "created_at", "updated_at", "deleted_at", "is_archived","is_draft", "cover_image"]
         
     
     @transaction.atomic
@@ -21,6 +22,10 @@ class BlogPostSerializer(serializers.ModelSerializer):
         # validated_data["content"] = html_content
         markdown_content = markdown_file.read().decode("utf-8")
         validated_data["content"] = markdown_content
+        cover_image = validated_data.get("cover_image")
+        obj = self.upload_client.execute(cover_image, "MYBALANCE/COVER_IMAGE")
+        cover_image_url = obj["data"]["url"]
+        
 
         post= BlogPost.objects.create(**validated_data)
         post.is_draft = True
